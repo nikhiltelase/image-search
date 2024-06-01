c8256597bd555637eb7e23d1180049e5d09ad443
nikhil <nikhiltelase@gmail.com>
2024-06-01 09:23:33 +0530

    add loader and error handling

--- script.js
+++ script.js
@@ -1,38 +1,76 @@
 var form = document.getElementById("form");
 var keyword = document.getElementById("keyword");
 var loadMore = document.getElementById("loadmore");
-var imageContainer = document.getElementById("imageContainer");
+var allImageContainer = document.getElementById("allImageContainer");
+var searchLoader = document.getElementById("searchLoader");
+var moreLoader = document.getElementById("moreLoader");
+var showError = document.getElementById("showError");
+
 
 const clientId = "LfJX1YXR2ivnAyUARlX41CGwfwtueJ1L2oXzKXiPK0U"
 
 var pages = 1;
+var query ;
 
+//get images from upsplash.com using api call
 async function getImages() {
-    let response = await fetch(`https://api.unsplash.com/search/photos?page=${pages}&query=${keyword.value}&client_id=${clientId}&per_page=10`);
-    let data = await response.json();
-    let result = data.results;
+    query = keyword.value;
+    console.log(query.length)
+    if (query.length == 0){
+        showError.innerText = "Enter a input for search images"
+        //loader
+        searchLoader.style.display = "none";
+        moreLoader.style.display = "none";
+    }else{
+        try {
+            let response = await fetch(`https://api.unsplash.com/search/photos?page=${pages}&query=${query}&client_id=${clientId}&per_page=10`);
+            console.log(response);
+            let allData = await response.json();
+            let result = allData.results;
+            // not found error
+            console.log(result.length)
+            if (result.length == 0){
+                console.log("no found");
+                showError.innerText = "Image Not Found";
+    
+            }
+            
+            result.forEach((imageData) => {
+                console.log(imageData);
+                let imgDiv = document.createElement("div");
+                imgDiv.classList.add("image-container");
+    
+                let img = document.createElement("img");
+                img.classList.add("image");
+                img.src = imageData.urls.small;
+                imgDiv.appendChild(img);
+    
+                let downloadBtn = document.createElement("button");
+                downloadBtn.classList.add("download-btn");
+                downloadBtn.innerHTML = `<img src="download-2-line.png" alt="">`;
+                downloadBtn.addEventListener("click", () => {
+                    downloadImage(imageData.urls.full, imageData.id);
+                });
+                imgDiv.appendChild(downloadBtn);
+    
+                allImageContainer.appendChild(imgDiv);
+            });
+        } catch (error) {
+            console.log(error)
+            showError.innerText = "Error: Check your network or Reconnecting to Wi-Fi";
+        } finally{
+            //loader
+            searchLoader.style.display = "none";
+            moreLoader.style.display = "none";
+        }
+    }
 
-    result.forEach((data) => {
-        let imgDiv = document.createElement("div");
-        imgDiv.classList.add("image-container");
+    
+    
 
-        let img = document.createElement("img");
-        img.classList.add("image");
-        img.src = data.urls.small;
-        imgDiv.appendChild(img);
-
-        let downloadBtn = document.createElement("button");
-        downloadBtn.classList.add("download-btn");
-        downloadBtn.innerHTML = `<img src="download-2-line.png" alt="">`;
-        downloadBtn.addEventListener("click", () => {
-            downloadImage(data.urls.full, data.id);
-        });
-        imgDiv.appendChild(downloadBtn);
-
-        imageContainer.appendChild(imgDiv);
-    });
 }
 
+// download image 
 function downloadImage(url, id) {
     fetch(url)
         .then(response => response.blob())
@@ -46,14 +84,21 @@ function downloadImage(url, id) {
         .catch(error => console.error('Error downloading the image:', error));
 }
 
+//form submit event
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    imageContainer.innerHTML = "";
+    searchLoader.style.display = "block";
+
+    allImageContainer.innerHTML = "";
+    showError.innerHTML = "";
+
     pages = 1;  // Reset pages to 1 when a new search is submitted
     getImages();
 });
 
+//load more images
 loadMore.addEventListener("click", () => {
     pages++;
+    moreLoader.style.display = "block"
     getImages();
 });
